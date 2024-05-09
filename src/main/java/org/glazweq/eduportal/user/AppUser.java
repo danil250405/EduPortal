108f549a4d87bda30cdcb1ae1942f840868df2f9
glazweq <danilg250405@gmail.com>
2024-05-09 22:37:59 +0200

    changes in the project structure + additions to the user entity and everything adjacent to it

---
+++ src/main/java/org/glazweq/eduportal/user/AppUser.java
@@ -0,0 +1,78 @@
+package org.glazweq.eduportal.user;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+
+@Data
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+public class AppUser implements UserDetails {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private boolean enabled;
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private AppUserRole appUserRole;
+
+//    @ManyToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)
+//    @JoinTable(
+//            name="user_roles",
+//            joinColumns={@JoinColumn(name="user_id", referencedColumnName="id")},
+//            inverseJoinColumns={@JoinColumn(name="role_id", referencedColumnName="id")})
+//    private List<Role> roles = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(appUserRole.name());
+        return Collections.singleton(authority);
+    }
+    @Override
+    public String getPassword() {
+        return password;
+    }
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+}
---
+++ src/main/java/org/glazweq/eduportal/user/AppUserRole.java
@@ -0,0 +1,10 @@
+package org.glazweq.eduportal.user;
+
+public enum AppUserRole {
+    USER,
+    ADMIN,
+    TEACHER,
+    SUPER_ADMIN,
+    CONTENT_CREATOR;
+
+}
