495d665a9ca84eb0c4805b688167b79294c2e7fb
glazweq <danilg250405@gmail.com>
2024-05-21 23:05:56 +0200

    style for upload + fix name for download file

--- src/main/java/org/glazweq/eduportal/education/subject_data/DataRestController.java
+++ src/main/java/org/glazweq/eduportal/education/subject_data/DataRestController.java
@@ -1,9 +1,10 @@
-package org.glazweq.eduportal.storage;
-
+package org.glazweq.eduportal.education.subject_data;
 
 
 import lombok.AllArgsConstructor;
 import org.glazweq.eduportal.education.subject.Subject;
+import org.glazweq.eduportal.storage.StorageService;
+import org.glazweq.eduportal.storage.file_metadata.FileMetadataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
@@ -16,10 +17,11 @@ import java.io.IOException;
 @RestController
 @RequestMapping("/file")
 @AllArgsConstructor
-public class StorageController {
+public class DataRestController {
 
 
-    private StorageService service;
+    private StorageService storageService;
+    FileMetadataService fileMetadataService;
 
 //    @PostMapping("/upload")
 //    public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file,
@@ -29,18 +31,14 @@ public class StorageController {
 
     @GetMapping("/download/{fileName}")
     public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) throws IOException {
-        byte[] data = service.downloadFile(fileName);
+        byte[] data = storageService.downloadFile(fileName);
+        String originalName = fileMetadataService.findFileByS3Name(fileName).getOriginalFileName();
         ByteArrayResource resource = new ByteArrayResource(data);
         return ResponseEntity
                 .ok()
                 .contentLength(data.length)
                 .header("Content-type", "application/octet-stream")
-                .header("Content-disposition", "attachment; filename=\"" + fileName + "\"")
+                .header("Content-disposition", "attachment; filename=\"" + originalName + "\"")
                 .body(resource);
     }
-
-//    @DeleteMapping("/delete/{fileName}")
-//    public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
-//        return new ResponseEntity<>(service.deleteFile(fileName), HttpStatus.OK);
-//    }
 }
---
+++ src/main/resources/static/js/filesUploading.js
@@ -0,0 +1,90 @@
+function updateFileList(input) {
+    const fileList = document.getElementById('file-list');
+    fileList.innerHTML = ''; // Очистить список перед обновлением
+
+    const files = input.files;
+
+    for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        const listItem = document.createElement('div');
+        listItem.textContent = file.name;
+        fileList.appendChild(listItem);
+    }
+
+    // Изменить текст в области для загрузки файлов
+    const fileDropText = document.querySelector('.file-drop-text');
+    if (files.length === 0) {
+        fileDropText.textContent = 'Drag and Drop';
+    } else {
+        fileDropText.textContent = `${files.length} file${files.length > 1 ? 's' : ''} selected`;
+    }
+}
+
+// Обработчики событий drag и drop
+const fileDropArea = document.querySelector('.file-upload-label');
+
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+    fileDropArea.addEventListener(eventName, preventDefaults, false);
+});
+
+function preventDefaults(e) {
+    e.preventDefault();
+    e.stopPropagation();
+}
+
+['dragenter', 'dragover'].forEach(eventName => {
+    fileDropArea.addEventListener(eventName, highlight, false);
+});
+
+['dragleave', 'drop'].forEach(eventName => {
+    fileDropArea.addEventListener(eventName, unhighlight, false);
+});
+
+function highlight(e) {
+    fileDropArea.classList.add('highlighted');
+}
+
+function unhighlight(e) {
+    fileDropArea.classList.remove('highlighted');
+}
+
+fileDropArea.addEventListener('drop', handleDrop, false);
+
+function handleDrop(e) {
+    const files = e.dataTransfer.files;
+    const fileInput = document.getElementById('file-drop');
+    fileInput.files = files;
+    updateFileList(fileInput);
+}
+
+/*<![CDATA[*/
+const maxFileSizeBytes = /*[[${@environment.getProperty('spring.servlet.multipart.max-file-size')}]]*/ 5000000;
+
+function checkFileSize(files) {
+    const messageDivElement = document.getElementById('message-div-window');
+    const submitButton = document.getElementById('submit-button');
+    messageDivElement.classList.remove('message-div_hidden-window');
+
+    let fileSizeValid = true;
+    let messageText = '';
+
+    for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        if (file.size > maxFileSizeBytes) {
+            fileSizeValid = false;
+            messageText += `Файл "${file.name}" превышает максимально допустимый размер (${maxFileSizeBytes / 1024 / 1024} МБ)\n`;
+        }
+    }
+
+    if (fileSizeValid) {
+        messageDivElement.textContent = 'Все файлы имеют допустимый размер';
+        submitButton.disabled = false; // Разрешить кнопку отправки
+    } else {
+        messageDivElement.textContent = messageText;
+        submitButton.disabled = true; // Запретить кнопку отправки
+    }
+}
+
+
+
+//del Modal
