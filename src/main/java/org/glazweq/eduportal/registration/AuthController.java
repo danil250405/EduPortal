ddc99e905ab7e7804955379270f7f6735c49ae24
glazweq <danilg250405@gmail.com>
2025-03-09 11:42:31 +0100

    new branch added

--- src/main/java/org/glazweq/eduportal/registration/AuthController.java
+++ src/main/java/org/glazweq/eduportal/registration/AuthController.java
@@ -16,6 +16,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class AuthController {
     AppUserService appUserService;
+
     RegistrationService registrationService;
     @GetMapping("/login")
     public String login() {
