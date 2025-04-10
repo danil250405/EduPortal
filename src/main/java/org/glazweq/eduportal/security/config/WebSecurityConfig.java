6e9e11bdbeeb0c723926a81276099b90643bb49e
WorkPC <danilg250405@gmail.com>
2025-04-10 16:59:22 +0200

    add role managment

--- src/main/java/org/glazweq/eduportal/security/config/WebSecurityConfig.java
+++ src/main/java/org/glazweq/eduportal/security/config/WebSecurityConfig.java
@@ -47,40 +47,42 @@ public class WebSecurityConfig  {
 
                         authorize
 
-                                .requestMatchers("/").permitAll()
-                                .requestMatchers("/login").permitAll()
+
+                                .requestMatchers("/login/**").permitAll()
                                 .requestMatchers("/error-page").permitAll()
 
                                 .requestMatchers("/registration").permitAll()
                                 .requestMatchers("/registration/save").permitAll()
                                 .requestMatchers("/registration/confirm").permitAll()
-//                                  TODO: optimize this shit
-                                .requestMatchers("/faculties").permitAll()
-                                .requestMatchers("/specialtiesAll").permitAll()
-                                .requestMatchers("/subjectsAll").permitAll()
-                                .requestMatchers("/faculties/{facultyAbbreviation}").permitAll()
-                                .requestMatchers("/faculties/{facultyAbbr}/{specialtyAbbr}").permitAll()
-                                .requestMatchers("/faculties/{facultyAbbr}/{specialtyAbbr}/{subjectAddr}").permitAll()
-
-                                .requestMatchers("/profile").permitAll()
 
 
+                                .requestMatchers("/profile").authenticated()
+                                .requestMatchers("/teachers").authenticated()
+                                .requestMatchers("/folders/**").authenticated()
+                                .requestMatchers("/folders/add").hasAnyAuthority("ADMIN", "TEACHER")
+                                .requestMatchers("/course/add-teacher").hasAnyAuthority("ADMIN", "TEACHER")
+                                .requestMatchers("/course/remove-teacher").hasAnyAuthority("ADMIN", "TEACHER")
+                                .requestMatchers("/admin/licenseRequests").hasAnyAuthority("ADMIN", "TEACHER")
+                                .requestMatchers("/admin/add-license").hasAnyAuthority("ADMIN", "TEACHER")
+                                .requestMatchers("/admin/remove-license").hasAnyAuthority("ADMIN", "TEACHER")
+                                .requestMatchers("/admin/delete-license").hasAnyAuthority("ADMIN")
+                                .requestMatchers("/course/add").hasAnyAuthority("ADMIN", "TEACHER")
+                                .requestMatchers("/course/delete").hasAnyAuthority("ADMIN", "TEACHER")
+                                .requestMatchers("/file/delete").hasAnyAuthority("ADMIN", "TEACHER")
+                                .requestMatchers("/file/upload").hasAnyAuthority("ADMIN", "TEACHER")
+                                .requestMatchers("/file/download/").authenticated()
+                                .requestMatchers("/file/view/").authenticated()
+                                .requestMatchers("/coursesAll").authenticated()
+                                .requestMatchers("/teachers").authenticated()
+                                .requestMatchers("/licenseRequests").authenticated()
+                                .requestMatchers("/teacher/**").authenticated()
 //                                TODO: permit only admins
-                                .requestMatchers("/faculties/add").permitAll()
-                                .requestMatchers("/faculties/delete").permitAll()
 
-                                .requestMatchers("/specialties/add").permitAll()
-                                .requestMatchers("/specialties/delete").permitAll()
-                                .requestMatchers("/specialtiesAll/delete").permitAll()
-                                .requestMatchers("/subject/add").permitAll()
-                                .requestMatchers("/subject/delete").permitAll()
-                                .requestMatchers("/subjectsAll/delete").permitAll()
-                                .requestMatchers("/subject/add-teacher").permitAll()
-                                .requestMatchers("/subject/remove-teacher").permitAll()
                                 .requestMatchers("/file/**").permitAll()
                                 .requestMatchers("/folders").permitAll()
                                 .requestMatchers("/course/add").permitAll()
-                                .requestMatchers("/**").permitAll()
+
+
 
                 ).formLogin(
                         form -> form
